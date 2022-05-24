9f994b124ad5d04f631dc07a60e8cb3dc412e414
Sara Rodriguez <s.rodriguez91s@hotmail.com>
2022-05-24 09:45:37 +0200

    ej acabado

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+
 // Recuperamos TODOS los elementos HTML que cumplen con la condición class="button"
-const buttons = document.querySelectorAll('.button');
+const buttons = document.querySelectorAll('.button'); // NodeList, especie de array
 const body = document.querySelector('body');
+
+// for (let i = 0; i < buttons.length; i++) {
+//     buttons[i].addEventListener("click", pintarFondo);
+// }
+
+// recorre todo el array de botones. Para cada botón añade un evento 'click'
+buttons.forEach(button => {
+    button.onclick = pintarFondo;
+});
+
+
+function pintarFondo(event) {
+    document.body.style.backgroundColor = event.target.id;
+}
